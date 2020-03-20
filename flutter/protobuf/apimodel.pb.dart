404a2a7f2b613fc2bf880008bda1af16ebe3745d
zawawahoge <zawawabis@gmail.com>
2020-03-21 04:07:49 +0900

    protoc apimodel

---
+++ flutter/protobuf/apimodel.pb.dart
@@ -0,0 +1,52 @@
+///
+//  Generated code. Do not modify.
+//  source: apimodel.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class Channel extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('Channel', package: const $pb.PackageName('apimodel'), createEmptyInstance: create)
+    ..aOS(1, 'id')
+    ..aOS(2, 'name')
+    ..hasRequiredFields = false
+  ;
+
+  Channel._() : super();
+  factory Channel() => create();
+  factory Channel.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Channel.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  Channel clone() => Channel()..mergeFromMessage(this);
+  Channel copyWith(void Function(Channel) updates) => super.copyWith((message) => updates(message as Channel));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static Channel create() => Channel._();
+  Channel createEmptyInstance() => create();
+  static $pb.PbList<Channel> createRepeated() => $pb.PbList<Channel>();
+  @$core.pragma('dart2js:noInline')
+  static Channel getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Channel>(create);
+  static Channel _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get id => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set id($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get name => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set name($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasName() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearName() => clearField(2);
+}
+
