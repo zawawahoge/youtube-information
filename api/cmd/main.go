12dfa6898a219eda54c712aff74485f73cccd514
zawawahoge <zawawabis@gmail.com>
2020-03-22 18:18:45 +0900

    replace echo server with grpc server

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,44 +1,28 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
+	"net"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"google.golang.org/grpc"
 
 	"github.com/zawawahoge/youtube-information/api/config"
 	"github.com/zawawahoge/youtube-information/api/factory"
-	"github.com/zawawahoge/youtube-information/api/handler"
+	"github.com/zawawahoge/youtube-information/api/protobuf/apiservice"
 	"github.com/zawawahoge/youtube-information/api/serviceimpl"
 )
-
-func corsConfig() middleware.CORSConfig {
-	return middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{http.MethodPost, http.MethodGet, http.MethodPut, http.MethodDelete},
-		AllowHeaders: []string{
-			echo.HeaderAuthorization,
-			echo.HeaderContentType,
-			echo.HeaderAcceptEncoding,
-			echo.HeaderAccept,
-			"X-Locale",
-		},
-		// Allow cache 1 hour. We don't make this too long so that this config applies
-		// soon when we update the api server.
-		MaxAge: 3600,
-	}
-}
 
 func main() {
 	youtubeConfig := config.MustConfigFromEnv()
 	youtube := serviceimpl.NewYoutubeServiceServer(youtubeConfig)
 	f := factory.New(youtube)
 
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.CORSWithConfig(corsConfig()))
-
-	handler.HandleCommonService(e, f.NewCommonServiceServer())
-
-	e.Logger.Fatal(e.Start(":8080"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 8080))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	apiservice.RegisterCommonServiceServer(grpcServer, f.NewCommonServiceServer())
+	log.Fatal(grpcServer.Serve(lis))
 }
