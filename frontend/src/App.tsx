79be8bb9c4a4026db3b1d932426b8a75595cace9
zawawahoge <zawawahoge@gmail.com>
2020-08-02 06:09:52 +0900

    use endpoint loaded from .env in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,28 +2,15 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import { ListSubscribedChannelsRequest, ListSubscribedChannelsResponse, Channel } from './proto/commonservice_pb';
 import { CommonServiceClient } from './proto/commonservice_grpc_web_pb';
-import { BookstoreClient } from './proto/bookstore_grpc_web_pb';
-import { ListBooksRequest, ListBooksResponse } from './proto/bookstore_pb';
 import ChannelList from './ChannelList';
+import config from './config';
 
 function App() {
   const [channels, setChannels] = useState<Channel[]>([]);
 
   useEffect(() => {
-    // const bookstoreService = new BookstoreClient("http://35.189.136.0")
-    // bookstoreService.listBooks(new ListBooksRequest(), {}, (err, resp: ListBooksResponse) => {
-    //   if (err) {
-    //     console.log(err.code);
-    //     console.log(err.message);
-    //   } else {
-    //     console.log(resp.getBooksList())
-    //     resp.getBooksList().forEach(b => {
-    //       console.log(b);
-    //     });
-    //   }
-    // })
-
-    const commonService = new CommonServiceClient("https://helical-disk-284416.uc.r.appspot.com");
+    console.log("Use API URL: " + config.APIUrl);
+    const commonService = new CommonServiceClient(config.APIUrl);
 
     const req = new ListSubscribedChannelsRequest();
     const call = commonService.listSubscribedChannels(req, {}, (err, resp: ListSubscribedChannelsResponse) => {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import * as dotenv from 'dotenv';
 
 ReactDOM.render(
   <App />,
