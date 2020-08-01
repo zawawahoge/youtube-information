4b026527e0c4a22928f7711c2bd6a706aaaee63c
zawawahoge <zawawahoge@gmail.com>
2020-08-02 04:48:33 +0900

    introduce app engine

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
+	"net/http"
+	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/reflection"
 
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/sirupsen/logrus"
 	"github.com/zawawahoge/youtube-information/api/config"
 	"github.com/zawawahoge/youtube-information/api/factory"
@@ -21,13 +24,30 @@ func main() {
 	youtube := serviceimpl.NewYoutubeServiceServer(youtubeConfig)
 	f := factory.New(youtube)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9090))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
 	grpcServer := grpc.NewServer()
 	apiservice.RegisterCommonServiceServer(grpcServer, f.NewCommonServiceServer())
 	reflection.Register(grpcServer)
-	logger.Println("gRPC server is initialized...")
-	log.Fatal(grpcServer.Serve(lis))
+	grpclog.SetLogger(log.New(os.Stdout, "exampleserver: ", log.LstdFlags))
+
+	wrappedGrpc := grpcweb.WrapServer(grpcServer)
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		allowCors(resp, req)
+		wrappedGrpc.ServeHTTP(resp, req)
+	}
+	port := 9090
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(handler),
+	}
+	logger.Printf("Start listening and serving at port = %d", port)
+	if err := httpServer.ListenAndServe(); err != nil {
+		logger.Fatal(err.Error())
+	}
+}
+
+func allowCors(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Access-Control-Allow-Origin", "*")
+	resp.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	resp.Header().Set("Access-Control-Expose-Headers", "grpc-status, grpc-message")
+	resp.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, XMLHttpRequest, x-user-agent, x-grpc-web, grpc-status, grpc-message")
 }
--- api/go.mod
+++ api/go.mod
@@ -3,9 +3,13 @@ module github.com/zawawahoge/youtube-information/api
 go 1.14
 
 require (
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/golang/protobuf v1.4.2
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/improbable-eng/grpc-web v0.13.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0 // indirect
 	golang.org/x/net v0.0.0-20200320220750-118fecf932d8 // indirect
